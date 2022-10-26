469584085c3ef0823ed370f74fab9b4ccc6e68ed
ajit-sf <ajiteshps@gmail.com>
2022-10-26 16:12:39 +0530

    OCR in progress

--- functions/ocrfunction/index.js
+++ functions/ocrfunction/index.js
@@ -13,7 +13,7 @@
 
 import { createRequire } from 'module';
 import { readFileSync } from 'fs';
-import fetch, {formData} from 'node-fetch';
+import fetch, { formData } from 'node-fetch';
 
 const require = createRequire(import.meta.url);
 
@@ -24,7 +24,7 @@ export default async function (event, context, logger) {
   // getting neccessary modules for JWT 
   const dotenv = require('dotenv');
   const jwt = require('jsonwebtoken');
-  const fs = require('fs'); 
+  const fs = require('fs');
 
 
   logger.info(`Invoking Ocrfunction with payload`);
@@ -33,24 +33,40 @@ export default async function (event, context, logger) {
   let payload = {
     "sub": 'ajiteshpratap.singh@salesforce.com',
     "aud": "https://api.einstein.ai/v2/oauth2/token",
-    "exp": (Math.floor( Date.now() / 1000) + 600), 
+    "exp": (Math.floor(Date.now() / 1000) + 600),
   };
 
   const key = readFileSync('einstein_platform_check.pem');
-  
+
   // Signing token
   const token = jwt.sign(payload, key, { algorithm: 'RS256' });
 
   // Getting access token to hit the API
   let response = await fetch('https://api.einstein.ai/v2/oauth2/token', {
-    method : 'POST',
-    headers : {
-      'Content-type' : 'application/x-www-form-urlencoded'
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded'
     },
     body: `grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=${token}`
   });
   let responseJSON = await response.json();
   console.log('Response ' + JSON.stringify(responseJSON));
+
+
+  // Fetching text values from images
+  const form = new FormData();
+  form.append('sampleLocation', 'https://www.publicdomainpictures.net/pictures/240000/velka/emergency-evacuation-route-signpost.jpg');
+  form.append('modelId', 'OCRModel');
+
+  let imageResponse = await fetch('https://api.einstein.ai/v2/vision/ocr', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${responseJSON.access_token}`
+    },
+    body: form
+  });
+  let imageJSON = await imageResponse.json();
+  console.log('imageJSON ' + JSON.stringify(imageJSON));
 
   return null;
 }
