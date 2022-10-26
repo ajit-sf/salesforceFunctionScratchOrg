1567a8cac92495e731130f45c535edc8113dc704
ajit-sf <ajiteshps@gmail.com>
2022-10-26 15:57:15 +0530

    OCR in progress

--- functions/ocrfunction/index.js
+++ functions/ocrfunction/index.js
@@ -12,6 +12,7 @@
  */
 
 import { createRequire } from 'module';
+import { readFileSync } from 'fs';
 
 const require = createRequire(import.meta.url);
 
@@ -31,11 +32,13 @@ export default async function (event, context, logger) {
   let payload = {
     "sub": 'ajiteshpratap.singh@salesforce.com',
     "aud": "https://api.einstein.ai/v2/oauth2/token",
-    "exp": Math.floor( Date.now() / 1000) + 600, 
+    "exp": (Math.floor( Date.now() / 1000) + 600), 
   };
+
+  const key = readFileSync('einstein_platform_check.pem');
   
   // Signing token
-  const token = jwt.sign(payload, fs.readFileSync('einstein_platform_check.pem'));
+  const token = jwt.sign(payload, key, { algorithm: 'RS256' });
 
   console.log('token ' + token);
 
@@ -44,7 +47,7 @@ export default async function (event, context, logger) {
     headers : {
       'Content-type' : 'application/x-www-form-urlencoded'
     },
-    body: `grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhaml0ZXNocHJhdGFwLnNpbmdoQHNhbGVzZm9yY2UuY29tIiwiYXVkIjoiaHR0cHM6Ly9hcGkuZWluc3RlaW4uYWkvdjEvb2F1dGgyL3Rva2VuIiwiZXhwIjoxNjY2NzgwMTY0fQ.XYgeEStKykbPTB5DhxdTYzRn6Y1V9ha9NFzxqvVWY5HFas8ObwTu0olU6mdzfi6OIF0LOeijPr9TiVMJQ2MU5tuxLcK6NDdwFvwXrR0bd6ebQ-Ob3WCNrXmcjfQbTsCW9Qcz8H2_abrZsp7ogFrQ9qvHyY7Sec-6RwSHMe52ngtHhJxq331QyT_KM7ZMm8w4EE3sPYLTaZGpm7-I8TTnU6xy1ALKOE2lCRQYDjK2FE-cODHFt_k7OMyxjCxJ4Sb2hkJy5KJAMcspwjCZVS19wNohpvd17-6qI6lRbpOOWXAiDYdV9GJeLuhjz0wcjgDVX_nz23DJBpjlvLM7NJ-gHQ`
+    body: `grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=${token}`
   });
   let responseJSON = await response.json();
   console.log('Response ' + JSON.stringify(responseJSON));
