c26542822654c38ab7a38e6f1673dcf95cfb262c
ajit-sf <ajiteshps@gmail.com>
2022-11-24 14:48:09 +0530

    Making changes to backend OCR and apex class to support

---
+++ force-app/main/default/aura/TestApp/TestApp.app
@@ -0,0 +1,3 @@
+<aura:application extends="force:slds">
+    <c:filePreviewCmp contentDocumentId="069DK000001bMlJYAU"></c:filePreviewCmp>
+</aura:application>
--- functions/ocrfunction/index.js
+++ functions/ocrfunction/index.js
@@ -12,7 +12,6 @@
  */
 
 import { createRequire } from 'module';
-import { readFileSync } from 'fs';
 import fetch, { FormData } from 'node-fetch';
 
 const require = createRequire(import.meta.url);
@@ -23,12 +22,8 @@ export default async function (event, context, logger) {
     logger.info(`Invoking Ocrfunction with payload`);
 
     // getting neccessary modules for JWT 
-    const dotenv = require('dotenv');
     const jwt = require('jsonwebtoken');
-    const fs = require('fs');
 
-    const downloadableUrl = event.data.downloadableUrl;
-    const type = event.data.type;
     const username = event.data.username;
     const privateKey = event.data.privateKey;
     console.log('type ' + type);
@@ -59,80 +54,52 @@ export default async function (event, context, logger) {
     console.log('Response ' + JSON.stringify(responseJSON));
 
 
-    //Fetching text values from images
-    const form = new FormData();
-    // form.append('sampleLocation', 'https://www.publicdomainpictures.net/pictures/240000/velka/emergency-evacuation-route-signpost.jpg');
-    form.append('sampleLocation', downloadableUrl);
-    form.append('task', 'contact');
-    form.append('modelId', 'OCRModel');
 
-    let imageResponse = await fetch('https://api.einstein.ai/v2/vision/ocr', {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${responseJSON.access_token}`
-      },
-      body: form
-    });
-    let imageJSON = await imageResponse.json();
-    console.log('imageJSON ' + JSON.stringify(imageJSON));
-
-    if (type === 'aadhaarFront') {
-      // Name check regex for capturing names
-      const nameReg = /^[A-Z](?:[a-z]|\b[,.'-]\b)+(?: [A-Z](?:[a-z]|\b[,.'-]\b)+)*$/;
-      const aadhaarNumberRegFour = /^[0-9]{4}$/;
-      const aadhaarNumberRegFull = /^\d{4}\s\d{4}\s\d{4}$/;
+    let aadhaarCardFrontObj = await imageProcessing(event.data.aadhaarCardFrontUrl, responseJSON.access_token, 'aadhaarFront');
+    let aadhaarCardBackObj = await imageProcessing(event.data.aadhaarCardBackUrl, responseJSON.access_token, 'aadhaarBack');
+    let panCardObj = await imageProcessing(event.data.panCardUrl, responseJSON.access_token, 'panCard');
+    return {...aadhaarCardFrontObj, ...aadhaarCardBackObj, ...panCardObj};
+  }
+  catch (e) {
+    console.log('Error ' + e);
+    return null;
+  }
 
 
-      let nameVal;
-      let aadhaarVal = [];
-      let aadhaarEntireNum = '';
 
-      for (let i of imageJSON.probabilities) {
-        if (nameReg.test(i.label)) {
-          nameVal = i.label;
-        }
-        if (aadhaarNumberRegFour.test(i.label)) {
-          aadhaarVal.push(i);
-        }
+}
 
-        if (aadhaarNumberRegFull.test(i.label)) {
-          aadhaarEntireNum = i.label;
-        }
-      }
 
-      if (!aadhaarEntireNum) {
-        aadhaarVal.sort((a, b) => a.probability - b.probability || a.boundingBox.minX - b.boundingBox.minX);
-        for (let i = aadhaarVal.length - 1; i >= 0; i--) {
-          aadhaarEntireNum += aadhaarVal[i].label;
+const imageProcessing = async (downloadableUrl, access_token, type) => {
+  //Fetching text values from images
+  const form = new FormData();
+  // form.append('sampleLocation', 'https://www.publicdomainpictures.net/pictures/240000/velka/emergency-evacuation-route-signpost.jpg');
+  form.append('sampleLocation', downloadableUrl);
+  form.append('task', 'contact');
+  form.append('modelId', 'OCRModel');
 
-          if (aadhaarEntireNum.length >= 12) {
-            break;
-          }
-        }
-      }
+  let imageResponse = await fetch('https://api.einstein.ai/v2/vision/ocr', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${access_token}`
+    },
+    body: form
+  });
+  let imageJSON = await imageResponse.json();
 
-      return { nameVal: nameVal, aadhaarNum: aadhaarEntireNum };
-    }
-    else if(type === 'aadhaarBack'){
+  if(type === 'aadhaarFront'){
+    return aadhaarCardFront(imageJSON);
+  }
+  else if(type === 'aadhaarBack'){
+    return aadhaarCardBack(imageJSON);
+  }
+  else if(type === 'panCard'){
+    return panCardBack(imageJSON);
+  }
+}
 
-      let addressArray = [];
-      let address = '';
-
-      for (let i of imageJSON.probabilities) {
-        if(i.attributes.tag === 'ADDRESS'){
-          addressArray.push(i);
-        }
-        
-      }
-      addressArray.sort((a,b) => a.boundingBox.minY - b.boundingBox.minY || a.boundingBox.minX - b.boundingBox.minX);
-
-      for(let i of addressArray){
-        address += i.label;
-      }
-      return {address : address};
-    }
-    else{
-      const panCardRegex = /([A-Z]){5}([0-9]){4}([A-Z]){1}$/;
+const panCardBack =  (imageJSON) => {
+  const panCardRegex = /([A-Z]){5}([0-9]){4}([A-Z]){1}$/;
       const nameRegex = /^[A-Z \W]+$/;
       let panNumber = '';
       let nameArr = [];
@@ -150,19 +117,61 @@ export default async function (event, context, logger) {
         nameArr.sort((a,b) => a.boundingBox.minY - b.boundingBox.minY);
         nameVal = nameArr[0].label;
       }
-      return {panNum : panNumber, name : nameVal};
+      return {panNum : panNumber, panName : nameVal};
+}
+
+const aadhaarCardBack =  (imageJSON) => {
+  let addressArray = [];
+  let address = '';
+
+  for (let i of imageJSON.probabilities) {
+    if(i.attributes.tag === 'ADDRESS'){
+      addressArray.push(i);
+    }
+    
+  }
+  addressArray.sort((a,b) => a.boundingBox.minY - b.boundingBox.minY || a.boundingBox.minX - b.boundingBox.minX);
+
+  for(let i of addressArray){
+    address += i.label;
+  }
+  return {address : address};
+}
+
+const aadhaarCardFront =  (imageJSON) => {
+  // Name check regex for capturing names
+  const nameReg = /^[A-Z](?:[a-z]|\b[,.'-]\b)+(?: [A-Z](?:[a-z]|\b[,.'-]\b)+)*$/;
+  const aadhaarNumberRegFour = /^[0-9]{4}$/;
+  const aadhaarNumberRegFull = /^\d{4}\s\d{4}\s\d{4}$/;
 
 
+  let nameVal;
+  let aadhaarVal = [];
+  let aadhaarEntireNum = '';
 
+  for (let i of imageJSON.probabilities) {
+    if (nameReg.test(i.label)) {
+      nameVal = i.label;
+    }
+    if (aadhaarNumberRegFour.test(i.label)) {
+      aadhaarVal.push(i);
     }
 
-
-  }
-  catch (e) {
-    console.log('Error ' + e);
-    return null;
+    if (aadhaarNumberRegFull.test(i.label)) {
+      aadhaarEntireNum = i.label;
+    }
   }
 
+  if (!aadhaarEntireNum) {
+    aadhaarVal.sort((a, b) => a.probability - b.probability || a.boundingBox.minX - b.boundingBox.minX);
+    for (let i = aadhaarVal.length - 1; i >= 0; i--) {
+      aadhaarEntireNum += aadhaarVal[i].label;
 
+      if (aadhaarEntireNum.length >= 12) {
+        break;
+      }
+    }
+  }
 
+  return { aadhaarName: nameVal, aadhaarNum: aadhaarEntireNum };
 }
