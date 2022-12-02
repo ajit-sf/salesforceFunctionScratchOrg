63798ab650a9713932637a6988bc895f6d115a5a
ajit-sf <ajiteshps@gmail.com>
2022-12-02 14:47:09 +0530

    Functions setup

--- functions/kycvalidator/index.js
+++ functions/kycvalidator/index.js
@@ -11,9 +11,9 @@
  *                 to a given execution of a function.
  */
 
- import { Client } from "pg";
+ import { createRequire } from 'module';
 
- const ClientH = Client;
+ const require = createRequire(import.meta.url);
 
 export default async function (event, context, logger) {
   const username = event.data.creds.user;
@@ -23,14 +23,17 @@ export default async function (event, context, logger) {
   const port = event.data.creds.port;
 
   // // const client = new ClientH('postgres://mfpibdyonqmjlh:256381ccec702257f70e5d73cfe279a1a82bf8a0d4c68647ab5ba9cd51914511@ec2-54-173-77-184.compute-1.amazonaws.com:5432/d2ip5p67bmcspn');
-  const client = new ClientH({
+  const {Client} = require('pg');
+  const client = new Client({
     host : host,
     database : database,
     user : username,
     password : password,
     port : port
   });
+  console.log(client + 'client 123');
   await client.connect();
+  console.log(client + 'client 123');
   let res = await client.query("SELECT aadhaar_card_no, pan_card_no, cibil_score FROM kyc_validator");
   console.log(res + 'res 123');
 
