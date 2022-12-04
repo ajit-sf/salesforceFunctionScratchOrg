347a20984f98fb741f55234d51c454dd2075fac4
ajit-sf <ajiteshps@gmail.com>
2022-12-04 20:27:21 +0530

    inital validator setup

--- functions/kycvalidatorv1/index.js
+++ functions/kycvalidatorv1/index.js
@@ -24,11 +24,9 @@ export default async function (event, context, logger) {
 
   // const client = new ClientH('postgres://mfpibdyonqmjlh:256381ccec702257f70e5d73cfe279a1a82bf8a0d4c68647ab5ba9cd51914511@ec2-54-173-77-184.compute-1.amazonaws.com:5432/d2ip5p67bmcspn');
   const client = new ClientH({
-    host : host,
-    database : database,
-    user : username,
-    password : password,
-    port : port
+    connectionString: 'postgres://mfpibdyonqmjlh:256381ccec702257f70e5d73cfe279a1a82bf8a0d4c68647ab5ba9cd51914511@ec2-54-173-77-184.compute-1.amazonaws.com:5432/d2ip5p67bmcspn',
+  ssl: {
+    rejectUnauthorized: false
   });
   await client.connect();
   let res = await client.query("SELECT aadhaar_card_no, pan_card_no, cibil_score FROM kyc_validator");
